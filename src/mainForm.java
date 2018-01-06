d8bf61de11a8fe4bb6e7c4f75d47307038555e63
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2018-01-06 10:30:40 +0700

    Final
    Signed-off-by: davin <dbasudewa@gmail.com>

--- src/mainForm.java
+++ src/mainForm.java
@@ -25,46 +25,6 @@ public class mainForm extends Application {
     //final ObservableList options = FXCollections.observableArrayList();
     @Override
     public void start(Stage primaryStage) throws Exception {
-        try {
-
-            //fxmlLoader.setResources(ResourceBundle.getBundle("MyLocale", locale));
-            Parent root = FXMLLoader.load(getClass().getResource("/ui/mainMenu.fxml"));//
-            primaryStage.setTitle("Prj1_4Jan");
-            Scene scene = new Scene(root, 960, 600);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        }
-        catch (IOException e){
-            System.out.println(e);
-        }
-    }
-
-}
-
-/*
- * Copyright (c) $today.year.Davin Alfarizky Putra Basudewa.This software is for educational only
- */
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.*;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import static javax.swing.UIManager.getString;
-
-
-public class mainForm extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-    //final ObservableList options = FXCollections.observableArrayList();
-    @Override
-    public void start(Stage primaryStage) throws Exception {
         //isiComboBox();
         Parent root = FXMLLoader.load(getClass().getResource("/ui/mainMenu.fxml"));//
         primaryStage.setTitle("Prj1_4Jan");
@@ -78,27 +38,4 @@ public class mainForm extends Application {
         primaryStage.show();
 
     }
-
-    /*
-    public void isiComboBox(){
-        try{
-
-            Class.forName("driver.com.davin.connector");
-            Connection con = null;
-            Statement stmt = null;
-            ResultSet rs = null;
-            con = connector.setConnection();
-            stmt = con.createStatement();
-            rs = stmt.executeQuery("SELECT code_part FROM partlist;");
-
-            while(rs.next()){
-                options.add((rs.getString("code_part")));
-            }
-        }
-        catch (Exception e){
-
-        }
-    }
-*/
 }
-
